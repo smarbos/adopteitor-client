c9c2de5eba5787291f3ded60cbb8816454a91eb7
smarbos <smarbos@gmail.com>
2016-04-05 20:47:07 -0300

    Configured api end point for local and production variables

--- app/js/config.js
+++ app/js/config.js
@@ -2,10 +2,10 @@
 
 angular.module('config', [])
   .constant('ENV', {
+
     'name': 'development',
-    'apiEndpoint': 'http://localhost:8000',
-    'viewerURL': 'http://localhost:8002/'
-    // Staging
-    //'apiEndpoint': 'http://socialdomo.herokuapp.com/',
-    //'viewerURL': 'http://socialdomo.enbanquito.com.ar/viewers/'
+    'apiEndpoint': 'http://www.adopteitor.local:8080',
+    //'apiEndpoint': 'http://adopteitor.mutantlab.com.ar:8080',
+    //'apiEndpoint': 'http://adopteitor.mutantlab.com.ar:8080',
+
   });
