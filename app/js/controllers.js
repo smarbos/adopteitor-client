9d4de3aaea8d68ae30e948f4b7745f439732d5dc
smarbos <smarbos@gmail.com>
2016-04-08 23:12:26 -0300

    Ahora se muestran y registran los retornos del PHPmailer

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -209,71 +209,36 @@ adopteitorApp.controller('formularioAdopcion', ['$scope', '$uibModalInstance', '
 
 adopteitorApp.controller('transitos', ['$scope', 'FormularioAdopcion', '$http',
     function($scope, FormularioAdopcion, $http) {
-        $scope.result = 'hidden'
+        $scope.success = false;
         $scope.resultMessage;
         $scope.formData; //formData is an object holding the name, email, subject, and message
-        $scope.submitButtonDisabled = false;
         $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
         $scope.newFormularioAdopcion;
-        // $scope.newFormularioAdopcion.nombre="carlitos";
         $scope.save = function(contactform, formData) {
-            console.log(contactform);
-            console.log(formData);
-            console.log("Submit");
-
-            // var newFormularioAdopcion = new FormularioAdopcion(formularioAdopcion);
-            // console.log(newFormularioAdopcion);
-            // console.log(formularioAdopcion);
-    		// newFormularioAdopcion.$save(
-            //     function(formularioAdopcion, putResponseHeaders) {
-            //         $scope.FormulariosAdopcion.push(formularioAdopcion);
-            //         console.log("SAVED OK");
-            //     },
-            //     function(err){
-            //         console.log(err);
-            //     }
-            // );
-
+            $scope.submitted = true;
 
             event.preventDefault();
-            $scope.submitted = true;
             $scope.submitButtonDisabled = true;
             if (formData.$valid) {
-                console.log("Valid"+formData.$valid);
-                console.log(formData);
-                console.log($.param(contactform));
                 $http({
                     method  : 'POST',
                     url     : 'transito-send-mail.php',
-                    data    : $.param(contactform),  //param method from jQuery
+                    data    : $.param($scope.formData),  //param method from jQuery
                     headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
                 }).success(function(data){
-                    if (data.success) { //success comes from the return json object
-                        console.log("success");
-                        console.log(data);
+                    if (data.success) {
                         $scope.submitButtonDisabled = true;
-                        $scope.resultMessage = data.message;
-                        $scope.result='bg-success';
+                        $scope.success = true;
                     } else {
-                        console.log(data);
-                        console.log(data.message);
-                        console.log("failure");
+                        console.error("failure");
+                        $scope.submitted = false;
                         $scope.submitButtonDisabled = false;
-                        $scope.resultMessage = data.message;
-                        $scope.result='bg-danger';
                     }
+                    $scope.resultMessage = data.message;
                 });
             } else {
-                console.log("invalid");
-                $scope.submitButtonDisabled = false;
-                $scope.resultMessage = 'Failed <img src="http://www.chaosm.net/blog/wp-includes/images/smilies/icon_sad.gif" alt=":(" class="wp-smiley">  Please fill out all the fields.';
-                $scope.result='bg-danger';
+                $scope.submitted = false;
             }
-            console.log(formData);
-        }
-
-        $scope.cancel = function () {
-            $modalInstance.dismiss('cancel');
         }
     }
 ]);
