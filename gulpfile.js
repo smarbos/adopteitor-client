cb7815a5ce96ef7fdb9d14fb1217cccf103dc22f
smarbos <smarbos@gmail.com>
2016-04-19 22:59:37 -0300

    Unified all sass files into stylesheet.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer');
 
-var config = {
-    sassPath: './app/scss',
-    bowerDir: './bower_components'
+var output = "app/css/";
+var input = "app/scss/*.scss";
+
+var sassOptions = {
+  errLogToConsole: false,
+  outputStyle: 'expanded'
 };
-gulp.task('watch', function() {
-    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
-});
 
 gulp.task('css', function() {
-    return sass(config.sassPath + '/*.scss', {
+    return sass(config.sassPath + output, {
             style: 'compressed',
             loadPath: [
                 './app/scss',
@@ -22,9 +24,25 @@ gulp.task('css', function() {
 });
 
 gulp.task('sass', function() {
-  return gulp.src('/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('/app/css'));
+  return gulp
+    .src(input)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./stylesheets/maps'))
+    .pipe(gulp.dest(output));
 });
 
-gulp.task('default', ['watch']);
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch(input, ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('default', ['sass', 'watch']);
