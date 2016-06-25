216a9673062a4eb0e46411e38dc05b2631865550
smarbos <smarbos@gmail.com>
2016-06-25 19:09:47 -0300

    Removed dynamic generated files

--- app/dist/js/controllers/donaciones.min-debug.js
+++
@@ -1,9 +0,0 @@
-
-//------------------------------------------------------------------------------------------------------------//
-function donaciones($scope, $location, getAnimalByID, $stateParams, ENV, $http, $state, sliderService) {
-    console.log("DONACIONES");
-}
-donaciones.$inject = ['$scope', '$location', 'getAnimalByID', '$stateParams', 'ENV', '$http', '$state', 'sliderService'];
-adopteitorApp.controller('donaciones', donaciones);
-
-//------------------------------------------------------------------------------------------------------------//
