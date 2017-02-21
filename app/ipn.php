dc7523fcab15f1807c87fdd35fe92b33ecdd79ee
Damian Mantuano <damianm@camba.coop>
2017-02-21 19:12:01 -0300

    added ipn.php to save data from mercadopago

---
+++ app/ipn.php
@@ -0,0 +1,20 @@
+<?php
+
+$object = [ "content" => json_encode($_GET) ];
+
+$url = 'http://www.adoptaungalgoenargentina.com:8080/Ipn/';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, TRUE);
+curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $object);
+$head = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+die(json_encode($head));
+
+?>
